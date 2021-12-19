98dd2a22eccf78fe4734439d1cff4713cd1e6ebd
Shirodeep <shirodeepdahal0987@gmail.com>
2021-12-19 14:24:50 +0545

    Finished

---
+++ game.cpp
@@ -0,0 +1,150 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+char field[3][3] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+int i, j, playerInputValue;
+
+void displayGame()
+{
+    system("CLS");
+    // Displaying the output of game
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            cout << field[i][j] << ' ';
+        }
+        cout << endl;
+    }
+}
+
+void gameBody(int count)
+{
+    char playerDisplayValue = 'O';
+
+    cout << "Enter the field you want to mark:";
+    cin >> playerInputValue;
+    // Switching players
+    if (count % 2 == 0)
+    {
+        playerDisplayValue = 'X';
+    }
+
+    else
+    {
+        playerDisplayValue = 'O';
+    }
+    ////placing 'X' and 'O' for game
+    switch (playerInputValue)
+    {
+    case 1:
+        field[0][0] = playerDisplayValue;
+        break;
+    case 2:
+        field[0][1] = playerDisplayValue;
+        break;
+    case 3:
+        field[0][2] = playerDisplayValue;
+        break;
+    case 4:
+        field[1][0] = playerDisplayValue;
+        break;
+    case 5:
+        field[1][1] = playerDisplayValue;
+        break;
+    case 6:
+        field[1][2] = playerDisplayValue;
+        break;
+    case 7:
+        field[2][0] = playerDisplayValue;
+        break;
+    case 8:
+        field[2][1] = playerDisplayValue;
+        break;
+    case 9:
+        field[2][2] = playerDisplayValue;
+        break;
+    };
+}
+char checkGame()
+{
+    // Checking who won either 'O' or 'X'
+    // if 'X' won
+    ////Checking diagonals
+    if (field[0][0] == 'X' && field[1][1] == 'X' && field[2][2] == 'X')
+        return 'X';
+    if (field[0][2] == 'X' && field[1][1] == 'X' && field[2][0] == 'X')
+        return 'X';
+    //// Chcking columns
+    if (field[0][0] == 'X' && field[1][0] == 'X' && field[2][0] == 'X')
+        return 'X';
+    if (field[0][1] == 'X' && field[1][1] == 'X' && field[2][1] == 'X')
+        return 'X';
+    if (field[0][2] == 'X' && field[1][2] == 'X' && field[2][2] == 'X')
+        return 'X';
+    //// Checking rows
+    if (field[0][0] == 'X' && field[0][1] == 'X' && field[0][2] == 'X')
+        return 'X';
+    if (field[1][0] == 'X' && field[1][1] == 'X' && field[1][2] == 'X')
+        return 'X';
+    if (field[2][0] == 'X' && field[2][1] == 'X' && field[2][2] == 'X')
+        return 'X';
+
+    // If 'O' won
+    //// Checing Diagonals
+    if (field[0][0] == 'O' && field[1][1] == 'O' && field[2][2] == 'O')
+        return 'O';
+    if (field[0][2] == 'O' && field[1][1] == 'O' && field[2][0] == 'O')
+        return 'O';
+    //// Checking Columns
+    if (field[0][0] == 'O' && field[1][0] == 'O' && field[2][0] == 'O')
+        return 'O';
+    if (field[0][1] == 'O' && field[1][1] == 'O' && field[2][1] == 'O')
+        return 'O';
+    if (field[0][2] == 'O' && field[1][2] == 'O' && field[2][2] == 'O')
+        return 'O';
+    //// Checking Rows
+    if (field[0][0] == 'O' && field[0][1] == 'O' && field[0][2] == 'O')
+        return 'O';
+    if (field[1][0] == 'O' && field[1][1] == 'O' && field[1][2] == 'O')
+        return 'O';
+    if (field[2][0] == 'O' && field[2][1] == 'O' && field[2][2] == 'O')
+        return 'O';
+
+    return 'E';
+}
+
+int main()
+{
+    // initializing counter for toggling player
+    int count = 0;
+
+    cout << endl
+         << "\t\tWelcome to TICTACTOE -v 1.0" << endl;
+    displayGame();
+    // Infinite loop for game
+    while (1)
+    {
+        gameBody(count);
+        displayGame();
+        if (count == 9)
+        {
+            cout << "DRAW!!!";
+            break;
+        }
+        else if ('O' == checkGame())
+        {
+            cout << "PLAYER 'O' WON!!!";
+            break;
+        }
+        else if ('X' == checkGame())
+        {
+            cout << "PLAYER 'X' WON!!!";
+            break;
+        }
+
+        count++;
+    }
+    return 0;
+}
